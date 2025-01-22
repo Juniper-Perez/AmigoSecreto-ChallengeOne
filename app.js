12304579244b11f34599f02593d323167ff55e56
Juniper-Perez <juniper.perez46@gmail.com>
2025-01-22 18:30:54 -0300

    Adicionando nomes a lista, limpando input e alerta se input vazio

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-//Olá. Esse é o teste pra salvar no github
+let amigos = [];
+
+
+function adicionarAmigo() {
+    let nomeAmigo = document.querySelector('input').value;
+    let listaDeAmigos
+    if (nomeAmigo) {  // Verifica se o valor não está vazio
+        amigos.push(nomeAmigo);  
+        console.log (amigos)
+        listaDeAmigos = document.getElementById("listaAmigos").textContent = amigos.join(', ');
+    } else {
+        console.log ('Nome vazio');
+         alert ('Por Favor insira um nome');
+    }
+    limparNome()
+}
+
+function limparNome() {
+    let nomeAmigo = document.querySelector('input')
+    nomeAmigo.value = '';
+} 
+
+
